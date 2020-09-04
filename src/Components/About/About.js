4fbd32d4ae94c6a6ea9f269bd3a0e137866af4dc
stefanghadjiew <stefanghadjiew@abv.bg>
2020-09-05 01:21:58 +0300

    about component almost finished.Note:css transitions dont work with conditional rendering when using ternary operator(react).

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,21 +1,75 @@
 import React from "react";
-import classes from "./About.module.css"
 import { SiHtml5,SiCss3,SiJavascript,SiReact,SiMongodb } from "react-icons/si"
 import { FaNodeJs } from "react-icons/fa"
+import { BsHeartFill } from "react-icons/bs"
+import classNames from "classnames"
+import "./About.css"
+
 
 
 const About = () => {
     return (
-        <div className={classes.about}>
-            <h2>My journey began march of 2020.<br/>
-            My companions : <SiHtml5 className={classes.html}/> <SiCss3/> <SiJavascript/> <SiReact/><br/>
-            Endgame : &#123;...My companions,<FaNodeJs />,<SiMongodb/>,&#125;<br/>
-            I love the process of learning.<br/>
-            I might even be addicted to it ...<br/>
-            The technologies themselves are not that important       
-            </h2>
+        <div>
+            <div className="about">
+                <div className="h2">
+                    Journey
+                    <div 
+                    className="after-journey-hover">
+                    My journey began march of 2020.
+                    With the help of a friend,i dove into the web-developer world.
+                    Loved every minute of it!Hope i`ll be able to do this for the rest of my life
+                    </div> 
+                </div>
+                <div className="h2">
+                    companions
+                    <div className="after-journey-hover">
+                        <SiHtml5 className={classNames('icon','html')}/>  
+                        <SiCss3 className={classNames('icon','css')}/> 
+                        <SiJavascript className={classNames('icon','js')}/> 
+                        <SiReact className={classNames('icon','react')}/>
+                    </div>
+                </div>
+                <div
+                    
+                    className="h2"
+                    >
+                    endgame
+                </div>
+                    <div
+                    
+                    className="h2"
+                    >
+                    passion
+                </div>
+                <div
+                  
+                    className="h2"
+                    >
+                    tech
+                </div>
+               
+               
+                
+            {/*  <
+                <h2 className="h2">My companions :</h2> 
+                    <SiHtml5 className={classNames('icon','html')}/>  
+                    <SiCss3 className={classNames('icon','css')}/> 
+                    <SiJavascript className={classNames('icon','js')}/> 
+                    <SiReact className={classNames('icon','react')}/>
+                <h2 className="h2">Endgame : &#123;...My companions,<FaNodeJs className={classNames('icon','node')} />,<SiMongodb className={classNames('icon','mongo')}/>,&#125;</h2>
+                <h2 className="h2">I <BsHeartFill className={classNames('icon','heart')}/> the process of learning.</h2>
+                <h2 className="h2">I might even be addicted to it ...</h2>
+                <h2 className="h2">The technologies themselves are not that important</h2> */}
+            </div>
+            
+           
+
+            
+           
         </div>
     )
 }
 
 export default About;
+
+
