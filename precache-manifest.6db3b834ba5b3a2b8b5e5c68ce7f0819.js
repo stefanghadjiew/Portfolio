ca406ce017c275d114c94febba1f6bcd0a917977
stefanghadjiew <stefanghadjiew@abv.bg>
2020-10-16 13:58:09 +0300

    Updates

---
+++ precache-manifest.6db3b834ba5b3a2b8b5e5c68ce7f0819.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2af74e903fdc1ce575a33da209a43e2d",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "2158eddcca3ce93cb406",
+    "url": "/Portfolio/static/css/main.d2b6389e.chunk.css"
+  },
+  {
+    "revision": "f0e69b5e068182ec1bda",
+    "url": "/Portfolio/static/js/2.e71ca021.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Portfolio/static/js/2.e71ca021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2158eddcca3ce93cb406",
+    "url": "/Portfolio/static/js/main.612626fa.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "df65651bbe6711a0507a3ab52a837b56",
+    "url": "/Portfolio/static/media/background1.df65651b.jpg"
+  },
+  {
+    "revision": "a0fb51ab4b88c00165c7592ff5465059",
+    "url": "/Portfolio/static/media/homeImage.a0fb51ab.jpg"
+  },
+  {
+    "revision": "e0729e9f0f8ce066dde175453d796931",
+    "url": "/Portfolio/static/media/wallhaven-45kmo3.e0729e9f.jpg"
+  },
+  {
+    "revision": "3a173aea5726f0490ceb691794783861",
+    "url": "/Portfolio/static/media/wallhaven-kw1o87.3a173aea.jpg"
+  },
+  {
+    "revision": "b16e9ee93afbf1a19d272964663e16a3",
+    "url": "/Portfolio/static/media/wallhaven-nmx16y.b16e9ee9.jpg"
+  },
+  {
+    "revision": "2f4322dac2c69918d11c975dbe0de3c2",
+    "url": "/Portfolio/static/media/wallhaven-nzz9pv.2f4322da.jpg"
+  }
+]);
